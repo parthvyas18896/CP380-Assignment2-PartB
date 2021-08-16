0f7312ce37ea68f8f1b8b3a9fc814d46d0c5845f
parthvyas18896 <pvyas1@confederationcollege.ca>
2021-08-16 15:46:58 -0400

    added assignment but still need to fix one error

---
+++ CP380-B2-BlockWebAPI/Controllers/PendingPayloadsController.cs
@@ -0,0 +1,37 @@
+﻿using CP380_B1_BlockList.Models;
+using CP380_B2_BlockWebAPI.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CP380_B2_BlockWebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [Produces("application/json")]
+    public class PendingPayloadsController : ControllerBase
+    {
+        // TODO
+        private readonly PendingPayloads pendingPayload;
+        public PendingPayloadsController(PendingPayloads pendingPayloads)
+        {
+            pendingPayload = pendingPayloads;
+        }
+
+        [HttpGet("/current")]
+        public ActionResult Get()
+        {
+            return Ok(pendingPayload.Payloads);
+        }
+
+        [HttpPost("/add")]
+        public ActionResult Post(Payload payload)
+        {
+            pendingPayload.Payloads.Add(payload);
+            return Ok();
+        }
+    }
+}
